4efc38a0a464e3044330d9fba835863f86dc167e
AJ-Gamer <ajohnsonjr0405@gmail.com>
2024-09-25 11:10:08 -0500

    Added styling for NavBar and App components

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,17 +1,36 @@
 import React, { FC } from 'react';
+import { Box, Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const NavBar: FC = () => (
-  <nav>
-    <ul>
-      <li><Link to="/home">Home</Link></li>
-      <li><Link to="/char-creation">Character Creation</Link></li>
-      <li><Link to="/encounters">Encounters</Link></li>
-      <li><Link to="/inventory">Inventory</Link></li>
-      <li><Link to="/map-gen">Map Generator</Link></li>
-      <li><Link to="/store">Store</Link></li>
-    </ul>
-  </nav>
+  <Box 
+    as="nav" 
+    bg="gray.800" 
+    color="black" 
+    px={4} 
+    py={2} 
+    position="fixed" 
+    width="100%"
+    height="8%"
+    top={0} 
+    zIndex={1000}
+  >
+    <Box position="absolute" right={4}>
+      <Menu>
+        <MenuButton as={Button} bg="gray.500">
+          Menu
+        </MenuButton>
+        <MenuList>
+          <MenuItem as={Link} to="/home">Home</MenuItem>
+          <MenuItem as={Link} to="/char-creation">Character Creation</MenuItem>
+          <MenuItem as={Link} to="/encounters">Encounters</MenuItem>
+          <MenuItem as={Link} to="/inventory">Inventory</MenuItem>
+          <MenuItem as={Link} to="/map-gen">Map Generator</MenuItem>
+          <MenuItem as={Link} to="/store">Store</MenuItem>
+        </MenuList>
+      </Menu>
+    </Box>
+  </Box>
 );
 
 export default NavBar;

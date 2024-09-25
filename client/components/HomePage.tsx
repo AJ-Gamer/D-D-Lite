f16b9e2821a19e963de2be55bd8bd44fd9f62cb9
AJ-Gamer <ajohnsonjr0405@gmail.com>
2024-09-25 11:10:23 -0500

    Updated basic styling for components

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react';
+import { Box, Text, Image } from '@chakra-ui/react';
 
 const HomePage: FC = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <h1>This page is under construction!</h1>
-        <img 
-            src="https://cdn.mos.cms.futurecdn.net/Z7Rv4K4aVfzDzJnHPJXPF8.gif" 
-            alt="Under Construction"
-        />
-    </div>
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="80vh">
+      <Image src="https://cdn.mos.cms.futurecdn.net/Z7Rv4K4aVfzDzJnHPJXPF8.gif" alt="Under Construction" boxSize="300px" />
+      <Text fontSize="2xl" textAlign="center" mt={4}>
+        This page is under construction!
+      </Text>
+    </Box>
   );
 };
 

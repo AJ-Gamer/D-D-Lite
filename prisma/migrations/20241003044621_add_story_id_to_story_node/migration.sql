e3cd48acfb9aed28606c2fa53b945201a8102586
AJ-Gamer <ajohnsonjr0405@gmail.com>
2024-10-03 01:01:29 -0500

    Updated StoryNode Model

---
+++ prisma/migrations/20241003044621_add_story_id_to_story_node/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `storyId` to the `StoryNode` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `storynode` ADD COLUMN `storyId` INTEGER NOT NULL;

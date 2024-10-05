70d50da7c33e6a023a6470a88ad2a7fcadfad43f
AJ-Gamer <ajohnsonjr0405@gmail.com>
2024-10-05 17:39:31 -0500

    Updated Styling (Again)

--- client/components/CharCreation.tsx
+++ client/components/CharCreation.tsx
@@ -146,7 +146,7 @@ const CharCreation: FC<CharCreationProps> = ({ profile }) => {
         selectedChar={selectedChar}
         onDeleteChar={handleDeleteChar}
       />
-      <Box p={6} bg="#B8860B" alignContent="center" borderWidth="1px" borderRadius="lg">
+      <Box p={6} bg="#F6CC12" alignContent="center" borderWidth="1px" borderRadius="lg">
         <Customization
           charName={charName}
           setCharName={setCharName}
@@ -158,7 +158,7 @@ const CharCreation: FC<CharCreationProps> = ({ profile }) => {
           setCharClass={setCharClass}
         />
         <Button
-          bg="#B8860B"
+          bg="#F6CC12"
           mt={4}
           onClick={handleSubmit}
           isDisabled={!charName || !race || !charClass}

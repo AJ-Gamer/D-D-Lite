c6e592c482538c1f218b44378d052e85796c6c4f
mothroom <dakotaday1997@gmail.com>
2024-10-07 18:34:54 -0500

    UPDATE / Added optimizations to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const path = require('path');
+const { transpile } = require('typescript');
 
 const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/dist/client');
@@ -9,8 +12,11 @@ module.exports = {
   devtool: 'source-map',
   entry: `${SRC_DIR}/index.tsx`,
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: DIST_DIR,
+  },
+  cache: {
+    type: 'filesystem',
   },
   resolve: {
     modules: ['node_modules'],
@@ -21,7 +27,15 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: 'ts-loader',
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
         include: [SRC_DIR]
       },
     ],
@@ -31,5 +45,15 @@ module.exports = {
       template: path.resolve(SRC_DIR, 'index.html'),
       filename: './index.html',
     }),
+    new ForkTsCheckerWebpackPlugin(),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      parallel: true,
+    })],
+    splitChunks: {
+      chunks: 'all',
+    }
+  }
 };
